dd5680b5633c172b5ba92b77f849853e917840fd
Vipin Singh <svipin174@gmail.com>
2022-06-25 21:44:10 +0530

    making light dark theme method one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { MatSelectChange } from '@angular/material/select';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ang-material';
-  selected = 'option2';
+export class AppComponent implements OnInit {
+  @HostBinding('class')
+  currentTheme: 'light-theme' | 'dark-theme' = 'dark-theme';
+
+  constructor(private overlayContainer: OverlayContainer) {}
+
+  ngOnInit(): void {
+    this.currentTheme = 'dark-theme';
+    this.overlayContainer.getContainerElement().classList.add('dark-theme');
+    this.overlayContainer.getContainerElement().classList.remove('light-theme');
+  }
+
+  setTheme({ source }: MatSelectChange) {
+    if (source.value === 'light') {
+      this.currentTheme = 'light-theme';
+      this.overlayContainer
+        .getContainerElement()
+        .classList.remove('dark-theme');
+      this.overlayContainer.getContainerElement().classList.add('light-theme');
+    } else {
+      this.currentTheme = 'dark-theme';
+      this.overlayContainer.getContainerElement().classList.add('dark-theme');
+      this.overlayContainer
+        .getContainerElement()
+        .classList.remove('light-theme');
+    }
+  }
 }
