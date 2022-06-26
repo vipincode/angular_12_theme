772a9cbf62e3cd8cadde3d902aad86a2dfe941e1
Vipin Singh <svipin174@gmail.com>
2022-06-26 08:05:25 +0530

    fix: light dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  Inject,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 import { MatSelectChange } from '@angular/material/select';
 import { OverlayContainer } from '@angular/cdk/overlay';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -8,30 +15,23 @@ import { OverlayContainer } from '@angular/cdk/overlay';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  @HostBinding('class')
-  currentTheme: 'light-theme' | 'dark-theme' = 'dark-theme';
-
-  constructor(private overlayContainer: OverlayContainer) {}
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
-    this.currentTheme = 'dark-theme';
-    this.overlayContainer.getContainerElement().classList.add('dark-theme');
-    this.overlayContainer.getContainerElement().classList.remove('light-theme');
+    this.renderer.removeClass(this.document.body, 'light-theme');
+    this.renderer.addClass(this.document.body, 'dark-theme');
   }
 
   setTheme({ source }: MatSelectChange) {
     if (source.value === 'light') {
-      this.currentTheme = 'light-theme';
-      this.overlayContainer
-        .getContainerElement()
-        .classList.remove('dark-theme');
-      this.overlayContainer.getContainerElement().classList.add('light-theme');
+      this.renderer.removeClass(this.document.body, 'dark-theme');
+      this.renderer.addClass(this.document.body, 'light-theme');
     } else {
-      this.currentTheme = 'dark-theme';
-      this.overlayContainer.getContainerElement().classList.add('dark-theme');
-      this.overlayContainer
-        .getContainerElement()
-        .classList.remove('light-theme');
+      this.renderer.removeClass(this.document.body, 'light-theme');
+      this.renderer.addClass(this.document.body, 'dark-theme');
     }
   }
 }
